e0c4489df31de9a27a6036d8d3c23f87a43bc7cb
Ojas_ <114296963+ojas-sharma-01@users.noreply.github.com>
2024-07-12 16:34:52 +0530

    Update src.js

--- src.js
+++ src.js
@@ -7,7 +7,7 @@ let headersList = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36"
    }
    
-   let bodyContent = "f=&uid=2022UEA6517&pwd=qtxyal#1&HRAND_NUM=18421&fy=2024-25&comp=NETAJI SUBHAS UNIVERSITY OF TECHNOLOGY&cap=93867&logintype=student";
+   let bodyContent = "f=&uid=&HRAND_NUM=18421&fy=2024-25&comp=NETAJI SUBHAS UNIVERSITY OF TECHNOLOGY&cap=93867&logintype=student";
    
    let response = fetch("https://www.imsnsit.org/imsnsit/student_login110.php", { 
      method: "POST",
